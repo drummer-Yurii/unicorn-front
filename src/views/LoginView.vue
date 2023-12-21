46bc3a3bff7b016416f8ba70331c8fb77f4cf6ef
Yurii Rybachok <drummer2009@ukr.net>
2023-12-21 20:35:05 +0200

    create & styling the Login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,118 @@
+<template>
+  <form @submit.prevent="Login">
+    <h1>Login</h1>
+    <div class="form-group">
+      <label>Email: </label>
+      <input type="email" placeholder="Email" v-model="email" />
+    </div>
+    <div class="form-group">
+      <label>Password: </label>
+      <input type="password" placeholder="Password" v-model="password" />
+    </div>
+    <div>
+      <input type="submit" value="Login" />
+    </div>
+    <p>Need a account? <router-link to="/register">Register Here</router-link></p>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+import firebase from 'firebase/compat/app';
+
+export default {
+  setup() {
+    const email = ref('');
+    const password = ref('');
+
+    const Login = () => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value)
+        .then((data) => console.log(data))
+        .catch((err) => alert(err.message));
+    };
+
+    return {
+      Login,
+      email,
+      password,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: block;
+  width: 100%;
+  max-width: 400px;
+  padding: 50px 30px;
+  background-image: linear-gradient(to bottom, #00bc7e, #108775);
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+  border-radius: 16px;
+}
+
+form h1 {
+  color: #fff;
+  font-size: 36px;
+  text-transform: uppercase;
+  margin-bottom: 30px;
+}
+
+form .form-group {
+  margin-bottom: 30px;
+}
+
+form label {
+  display: block;
+  color: #eee;
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+
+form input {
+  appearance: none;
+  background: none;
+  outline: none;
+  border: none;
+}
+
+form input:not([type='submit']) {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+  transition: 0.4s;
+  color: #222;
+  font-size: 14px;
+  font-weight: 300;
+}
+
+form input:not([type='submit'])::placeholder {
+  color: #ccc;
+}
+
+form input:not([type='submit']):focus,
+form input:not([type='submit']):valid {
+  color: #fff;
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+}
+
+form input[type='submit'] {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  border-radius: 8px;
+  background-color: #2f4960;
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+  transition: 0.4s;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+</style>
