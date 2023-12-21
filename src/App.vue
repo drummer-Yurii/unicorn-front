aaff363f344b5d2e603467e775b691baa12448ca
Yurii Rybachok <drummer2009@ukr.net>
2023-12-21 20:32:21 +0200

    app with base styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
+<script>
+import { onBeforeMount } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import firebase from 'firebase/compat/app';
+export default {
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+
+    const auth = firebase.auth();
+
+    onBeforeMount(() => {
+      auth.onAuthStateChanged((user) => {
+        if (!user) {
+          router.replace('/login');
+        } else if (route.path == '/login' || route.path == '/register') {
+          router.replace('/');
+        }
+      });
+    });
+  },
+};
+</script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
+}
+
+body {
+  background: #2c3e50;
+  color: #fff;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
 }
 </style>
